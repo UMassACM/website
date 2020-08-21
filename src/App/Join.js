9ca24dee0d217d972e9067b45cf8b757d57421c8
Govind <govindchandak06@gmail.com>
2020-08-21 17:55:49 -0400

    Fixed iFrame alignment issues

--- src/App/Join.js
+++ src/App/Join.js
@@ -8,7 +8,7 @@ function Join({ routes }) {
         <div>
             <NaviBar />
             <div className="Join">
-                <iframe title="ACM Form Google" src="https://docs.google.com/forms/d/e/1FAIpQLSeHYany3qAYRxP_HGYg1EpfKw76DLx24Rtw-TJ5xUxUbERxGQ/viewform?embedded=true" width="640" height="860" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe class="Join-iframe" title="ACM Form Google" src="https://docs.google.com/forms/d/e/1FAIpQLSeHYany3qAYRxP_HGYg1EpfKw76DLx24Rtw-TJ5xUxUbERxGQ/viewform?embedded=true" width="640" height="860" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
             <BotNaviBar/>
         </div>
