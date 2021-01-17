15857a0d36d441886eecb9ef3a28d6bbd015b115
Saakshaat <saakshaat2001@gmail.com>
2021-01-18 01:29:53 +0530

    Add new component and add typography to officer cards

--- components/officerCard.js
+++ components/officerCard.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import { Card } from "semantic-ui-react";
 
+import styles from "../styles/utils.module.css"
+
 export default function OfficerCard({ name, title, img }) {
   return (
     <Card>
       <Image
         src={img}
         alt="Picture of the officer"
-        width={300}
-        height={500}
+        width={250}
+        height={400}
       />
-      <Card.Header>{title}</Card.Header>
-      <Card.Meta>{name}</Card.Meta>
+      <Card.Header className={styles.headingLg}>{title}</Card.Header>
+      <Card.Meta className={styles.headingMd}>{name}</Card.Meta>
     </Card>
   );
 }
