4a0edf6ae0d9cade7472927237d460c1ccb4ad8e
Saakshaat <saakshaat2001@gmail.com>
2021-01-18 09:57:45 +0530

    Update utilty typography

--- components/officerCard.js
+++ components/officerCard.js
@@ -1,17 +1,12 @@
 import Image from "next/image";
 import { Card } from "semantic-ui-react";
 
-import styles from "../styles/utils.module.css"
+import styles from "../styles/utils.module.css";
 
 export default function OfficerCard({ name, title, img }) {
   return (
     <Card>
-      <Image
-        src={img}
-        alt="Picture of the officer"
-        width={250}
-        height={400}
-      />
+      <Image src={img} alt="Picture of the officer" width={250} height={400} quality={10}/>
       <Card.Header className={styles.headingLg}>{title}</Card.Header>
       <Card.Meta className={styles.headingMd}>{name}</Card.Meta>
     </Card>
